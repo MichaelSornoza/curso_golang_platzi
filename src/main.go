b28b80f7adddaffc18a016dce55365e0397169ba
Michael Sornoza <michael.sornoza.ptm@snoopconsulting.com>
2022-05-11 17:37:34 -0500

    .

--- src/main.go
+++ src/main.go
@@ -1,50 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 
-	const squareBase = 10
-	squareArea := squareBase * squareBase
-	fmt.Println("The square Area is: ", squareArea)
+	fmt.Println("Hello, World!", math.Pi)
+	o := 8i
 
-	x := 30
-	y := 10
-
-	result := x + y
-	fmt.Println("The result is: ", result)
-
-	result = x - y
-	fmt.Println("The result of the subtraction is: ", result)
-
-	result = x * y
-	fmt.Println("The result of the multiplication is: ", result)
-
-	result = x / y
-	fmt.Println("The result of the division is: ", result)
-
-	result = x % y
-	fmt.Println("The result of the rest is: ", result)
-
-	result = x ^ y
-	fmt.Println("The result of the exponentiation is: ", result)
-
-	result = x & y
-	fmt.Println("The result of the bitwise AND is: ", result)
-
-	result = x | y
-	fmt.Println("The result of the bitwise OR is: ", result)
-
-	result++
-	fmt.Println("The result of the increment is: ", result)
-
-	result--
-	fmt.Println("The result of the decrement is: ", result)
-
-	result += y
-	fmt.Println("The result of the addition is: ", result)
-
-	result -= y
-	fmt.Println("The result of the subtraction is: ", result)
-
+	fmt.Printf("%T\n", o)
 }
